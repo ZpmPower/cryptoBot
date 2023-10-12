b22742d6a3153c528016d7089db6703ac0f38e98
Bohdan <sidorovbogdan1@gmail.com>
2023-10-12 17:58:56 +0200

    Create installDockerUbuntu.sh

---
+++ installDockerUbuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+echo "Docker has been successfully installed."
